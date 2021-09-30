da44e9256f7550ad9aeb4e76f45df6136399473d
Alexander J C <63383168+Jhuiice@users.noreply.github.com>
2021-09-30 06:42:35 -0600

    Removed Secret Key

--- week4/practiceDjango/praccyModels/praccyModels/settings.py
+++ week4/practiceDjango/praccyModels/praccyModels/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!09s43pzzac-do+cz(4f**i)0n&9jeoe+a9+4!ufnn+q%%jge^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
